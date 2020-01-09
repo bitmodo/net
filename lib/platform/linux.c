4bb881a2b30e874b7a8d578081f634337474f7d3
BSFishy <mttprvstanddad@gmail.com>
2020-01-09 18:55:06 +0000

    Formatting stuff

--- lib/platform/linux.c
+++ lib/platform/linux.c
@@ -22,9 +22,7 @@ int socketTypeToNetworkType(int type) {
 
 SocketData * initializeLinux(int side, int type) {
     int fd = socket(AF_INET, socketTypeToNetworkType(type), 0);
-    if (fd == -1) {
-        return NULL;
-    }
+    if (fd == -1) return NULL;
 
     SocketData * data = malloc(sizeof(SocketData));
     *data = (SocketData) {fd, -1};
@@ -33,66 +31,48 @@ SocketData * initializeLinux(int side, int type) {
 }
 
 int connectLinux(Socket * sock) {
-    if (!sock || !(sock->data) || sock->data->conn != -1) {
-        return ENULL_POINTER;
-    }
+    if (!sock || !(sock->data) || sock->data->conn != -1) return ENULL_POINTER;
 
     struct sockaddr_in addr;
     addr.sin_family = AF_INET;
     addr.sin_port = htons(sock->port);
 
     if (sock->address) {
-        if (inet_pton(AF_INET, sock->address, &(addr.sin_addr)) <= 0) {
-            return EINVALID_IP;
-        }
+        if (inet_pton(AF_INET, sock->address, &(addr.sin_addr)) <= 0) return EINVALID_IP;
     } else {
         return ENULL_POINTER;
     }
 
-    if (connect(sock->data->fd, (struct sockaddr *) &addr, sizeof(struct sockaddr_in)) < 0) {
-        return EUNKNOWN;
-    }
+    if (connect(sock->data->fd, (struct sockaddr *) &addr, sizeof(struct sockaddr_in)) < 0) return EUNKNOWN;
 
     return ESUCCESS;
 }
 
 int startLinux(Socket * sock) {
-    if (!sock || !(sock->data) || sock->data->conn != -1) {
-        return ENULL_POINTER;
-    }
+    if (!sock || !(sock->data) || sock->data->conn != -1) return ENULL_POINTER;
 
     struct sockaddr_in addr;
     addr.sin_family = AF_INET;
     addr.sin_port = htons(sock->port);
 
     if (sock->address) {
-        if (inet_pton(AF_INET, sock->address, &(addr.sin_addr)) <= 0) {
-            return EINVALID_IP;
-        }
+        if (inet_pton(AF_INET, sock->address, &(addr.sin_addr)) <= 0) return EINVALID_IP;
     } else {
         addr.sin_addr.s_addr = INADDR_ANY;
     }
 
-    if (bind(sock->data->fd, (struct sockaddr *) &addr, sizeof(struct sockaddr_in)) < 0) {
-        return EUNKNOWN;
-    }
+    if (bind(sock->data->fd, (struct sockaddr *) &addr, sizeof(struct sockaddr_in)) < 0) return EUNKNOWN;
 
-    if (listen(sock->data->fd, 10)) {
-        return EUNKNOWN;
-    }
+    if (listen(sock->data->fd, 10)) return EUNKNOWN;
 
     return ESUCCESS;
 }
 
 int loopLinux(Socket * sock) {
-    if (!sock || !(sock->data) || sock->data->conn != -1) {
-        return ENULL_POINTER;
-    }
+    if (!sock || !(sock->data) || sock->data->conn != -1) return ENULL_POINTER;
 
     int fd;
-    if ((fd = accept(sock->data->fd, NULL, 0)) < 0) {
-        return EUNKNOWN;
-    }
+    if ((fd = accept(sock->data->fd, NULL, 0)) < 0) return EUNKNOWN;
 
     sock->data->conn = fd;
 
@@ -100,33 +80,24 @@ int loopLinux(Socket * sock) {
 }
 
 int receiveLinux(Socket * sock, void * buf, int count) {
-    if (!sock || !(sock->data) || (sock->side == SERVER && sock->data->conn == -1)) {
-        return ENULL_POINTER;
-    }
+    if (!sock || !(sock->data) || (sock->side == SERVER && sock->data->conn == -1)) return ENULL_POINTER;
 
     int size;
-    if ((size = recv(sock->side == SERVER ? sock->data->conn : sock->data->fd, buf, count, 0)) < 0) {
-        return EUNKNOWN;
-    }
+    if ((size = recv(sock->side == SERVER ? sock->data->conn : sock->data->fd, buf, count, 0)) < 0) return EUNKNOWN;
 
     return size;
 }
 
 int sendLinux(Socket * sock, const void * buf, int count) {
-    if (!sock || !(sock->data) || (sock->side == SERVER && sock->data->conn == -1)) {
-        return ENULL_POINTER;
-    }
+    if (!sock || !(sock->data) || (sock->side == SERVER && sock->data->conn == -1)) return ENULL_POINTER;
 
-    if (send(sock->side == SERVER ? sock->data->conn : sock->data->fd, buf, count, 0) < 0) {
-        return EUNKNOWN;
-    }
+    if (send(sock->side == SERVER ? sock->data->conn : sock->data->fd, buf, count, 0) < 0) return EUNKNOWN;
 
     return ESUCCESS;
 }
 
 int closeConnectionLinux(Socket * sock) {
     if (!sock || !(sock->data) || sock->data->conn == -1) return ENULL_POINTER;
-    // if (sock == NULL || sock->data == NULL || sock->data->conn == -1) return ENULL_POINTER;
 
     close(sock->data->conn);
     sock->data->conn = -1;
@@ -135,13 +106,9 @@ int closeConnectionLinux(Socket * sock) {
 }
 
 int closeLinux(Socket ** sock) {
-    if (!sock || !((*sock)->data)) {
-        return ENULL_POINTER;
-    }
+    if (!sock || !((*sock)->data)) return ENULL_POINTER;
 
-    if ((*sock)->data->fd != -1) {
-        close((*sock)->data->fd);
-    }
+    if ((*sock)->data->fd != -1) close((*sock)->data->fd);
 
     free((*sock)->data);
     free(*sock);
