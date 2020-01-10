c31ee7fe352c5412a2fff62e3d2eb0cbeb17c080
BSFishy <mttprvstanddad@gmail.com>
2020-01-10 20:19:24 +0000

    Add some comments to the test

--- test/basic/main.c
+++ test/basic/main.c
@@ -13,6 +13,7 @@
 void * server(void * data) {
     fprintf(stdout, "Starting server\n");
 
+    // Setup the socket
     Socket * sock = net_socket(SERVER, TCP);
     if (!sock) {
         fprintf(stderr, "Failed to create server socket\n");
@@ -26,6 +27,7 @@ void * server(void * data) {
         return NULL;
     }
 
+    // Listen for connections
     fprintf(stdout, "Server: Waiting for connections on %s:%d\n", net_getAddress(sock), net_getPort(sock));
     int lc;
     while ((lc = net_loop(sock)) == ESUCCESS) {
@@ -34,6 +36,7 @@ void * server(void * data) {
         for (int i = 0; i < 5; i++) {
             char buf[1024] = {0};
 
+            // Receive data from the connection
             int readSize = net_receive(sock, buf, sizeof(buf)-1);
             if (readSize == -1) {
                 fprintf(stderr, "Server: Error reading from the socket\n");
@@ -42,17 +45,20 @@ void * server(void * data) {
             } else {
                 fprintf(stdout, "Server: Received message:\n\t%s\n", buf);
 
+                // Close the socket if a "close" command is received
                 if (strcmp(buf, "close") == 0) {
                     fprintf(stdout, "Server: Closing socket\n");
                     net_close(&sock);
                     break;
                 }
 
+                // Send a "pong" back with the same message
                 fprintf(stdout, "Server: Sending pong\n");
                 net_send(sock, buf, readSize);
             }
         }
 
+        // Close the currently open connection
         fprintf(stdout, "Server: Closing connection\n");
         net_closeConnection(sock);
     }
@@ -69,6 +75,7 @@ void * server(void * data) {
 void * client(void * data) {
     fprintf(stdout, "Starting client\n");
 
+    // Setup the socket
     Socket * sock = net_socket(CLIENT, TCP);
     if (!sock) {
         fprintf(stderr, "Failed to create client socket\n");
@@ -78,16 +85,20 @@ void * client(void * data) {
     net_setAddress(sock, HOST);
     net_setPort(sock, PORT);
     fprintf(stdout, "Client: Establishing connection on %s:%d\n", net_getAddress(sock), net_getPort(sock));
+
+    // Connect to the server
     int code;
     if ((code = net_connect(sock)) != ESUCCESS) {
         fprintf(stderr, "Client: Failed to connect with code %d\n", code);
         return NULL;
     }
 
+    // Send a simple message
     char message[] = "This is a test";
     fprintf(stdout, "Client: Sending: %s\n", message);
     net_send(sock, message, (int) sizeof(message));
 
+    // Receive a pong message
     char buf[1024] = {0};
     int receiveSize = net_receive(sock, buf, sizeof(buf)-1);
     if(receiveSize == -1) {
@@ -98,10 +109,12 @@ void * client(void * data) {
         fprintf(stdout, "Client: Received message:\n\t%s\n", buf);
     }
 
+    // Send a close message
     char closeMessage[] = "close";
     fprintf(stdout, "Client: Sending close command\n");
     net_send(sock, closeMessage, sizeof(closeMessage));
 
+    // Close the socket
     net_close(&sock);
     fprintf(stdout, "Finishing client\n");
     return NULL;
