a9f1da1a8aef6c7fb3e26b23087563870d30f4cb
BSFishy <mttprvstanddad@gmail.com>
2020-01-20 15:10:50 +0000

    Finish cleanup test

--- test/net/cleanup.c
+++ test/net/cleanup.c
@@ -2,8 +2,15 @@
 
 #include <assert.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 extern NetHandler * gHandler;
+
+bool cleaned = false;
+
+void customCleanup() {
+    cleaned = true;
+}
 
 int main() {
     gHandler = NULL;
@@ -14,6 +21,12 @@ int main() {
     *gHandler = (NetHandler) {NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL};
     netCleanup();
     assert(gHandler == NULL && "Handler was not null after setting it then cleanup");
+
+    gHandler = malloc(sizeof(NetHandler));
+    *gHandler = (NetHandler) {NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, &customCleanup};
+    netCleanup();
+    assert(gHandler == NULL && "Handler was not null after setting it then cleanup");
+    assert(cleaned == true && "Cleanup function was not called whenever cleaning up");
 
     exit(EXIT_SUCCESS);
 }
