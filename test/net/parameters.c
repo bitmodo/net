c981a0550b1c601c316875355c1191ce6c338e24
BSFishy <mttprvstanddad@gmail.com>
2020-01-27 20:26:36 +0000

    Add address tests

--- test/net/parameters.c
+++ test/net/parameters.c
@@ -30,4 +30,80 @@ Socket * createEmptySocket() {
 
 // Address get and set tests
 
+typedef struct AddressParams {
+    Socket * sock;
+    char * address;
+} AddressParams;
 
+void cleanupAddressParam(struct criterion_test_params *ctp) {
+    for (size_t i = 0; i < ctp->length; ++i) {
+        struct AddressParams *tup = (struct AddressParams *) ctp->params + i;
+
+        if (tup->sock) {
+            cr_free(tup->sock);
+        }
+
+        if (tup->address) {
+            cr_free(tup->address);
+        }
+    }
+
+    cr_free(ctp->params);
+}
+
+#define ADDRESS_PARAMS                                                                                      \
+    const size_t size = 4;                                                                                  \
+    AddressParams *params = cr_malloc(sizeof(AddressParams) * size);                                        \
+                                                                                                            \
+    int pos = 0;                                                                                            \
+    for (int i = 0; i < 2; i++) {                                                                           \
+        for (int j = 0; j < 2; j++) {                                                                       \
+            char * address = NULL;                                                                          \
+            if (j != 0) {                                                                                   \
+                char * addrTmp = "test";                                                                    \
+                address = cr_malloc(sizeof(char) * strlen(addrTmp));                                        \
+                address = strcpy(address, addrTmp);                                                         \
+            }                                                                                               \
+                                                                                                            \
+            params[pos++] = (AddressParams) {.sock = (i == 0 ? NULL : createSocket()), .address = address}; \
+        }                                                                                                   \
+    }                                                                                                       \
+                                                                                                            \
+    return cr_make_param_array(AddressParams, params, size, cleanupAddressParam)
+
+ParameterizedTestParameters(address, set) {
+    ADDRESS_PARAMS;
+}
+
+ParameterizedTest(AddressParams * param, address, set) {
+    cr_log_info("Address set test with {sock: %p, address: %s}\n", param->sock, param->address);
+    netSetAddress(param->sock, param->address);
+
+    if (param->sock) {
+        if (param->address) {
+            cr_assert_str_eq(param->sock->address, param->address, "Set address did not set the correct address");
+        } else {
+            cr_assert_null(param->sock->address, "Set address did not set the address to null");
+        }
+    }
+}
+
+ParameterizedTestParameters(address, get) {
+    ADDRESS_PARAMS;
+}
+
+ParameterizedTest(AddressParams * param, address, get) {
+    cr_log_info("Address get test with {sock: %p, address: %s}\n", param->sock, param->address);
+
+    if (param->sock) {
+        param->sock->address = param->address;
+
+        if (param->address) {
+            cr_assert_str_eq(netGetAddress(param->sock), param->address, "Get address did not return correct address");
+        } else {
+            cr_assert_null(netGetAddress(param->sock), "Null address did not return null address");
+        }
+    } else {
+        cr_assert_null(netGetAddress(param->sock), "Null socket did not return null address");
+    }
+}
